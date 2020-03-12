40f5eb2e9a4f93d487dcbb9bbdf593297b72394c
Adil MOURAHI <adil.mourahi@gmail.com>
2020-03-12 14:09:45 +0100

    استقبال

--- src/app/pages/mobilite/receptioneleve/receptioneleve.component.ts
+++ src/app/pages/mobilite/receptioneleve/receptioneleve.component.ts
@@ -15,6 +15,8 @@ export class ReceptioneleveComponent implements OnInit {
   myecoles;
   arraytab = ['لائحة المؤسسات', 'استقبال التلاميذ'];
   dataeleves;
+  dataview;
+  filtreactive = {region: '', direction: '', commune: '', ecole: ''};
 
   constructor(private service: MysettingsService) { }
 
@@ -23,6 +25,8 @@ export class ReceptioneleveComponent implements OnInit {
     this.mylistEtablis = this.service.ListEtablis.filter( j => j.region == this.myacademies[0]);
     this.mydirections = [...new Set( this.mylistEtablis.filter( d => d.region == this.myacademies[0]).map( k => k.direction))];
     this.cycles = ['ابتدائي'];
+    this.filtreactive.region = this.myacademies[0];
+    this.dataview = [];
     this.dataeleves = this.prepareDataeleves();
   }
   prepareDataeleves() {
@@ -30,7 +34,7 @@ export class ReceptioneleveComponent implements OnInit {
     [...new Set(this.service.ListAutreEleves.map(i => i.gresa))].forEach(k => {
         const c = this.service.ListAutreEleves
         .filter(j => j.gresa == k );
-        if( c.length > 1){
+        if ( c.length > 1){
           let da;
           c.forEach(a => {
             if (da == undefined) {
@@ -56,7 +60,7 @@ export class ReceptioneleveComponent implements OnInit {
   }
 
   indexTab(e){
-    console.log("e=",e);
+    console.log("e=", e);
 
   }
 
@@ -66,16 +70,43 @@ export class ReceptioneleveComponent implements OnInit {
       case 'academies':
         this.mylistEtablis = this.service.ListEtablis.filter( j => j.region == t.value);
         this.mydirections = [...new Set( this.mylistEtablis.filter( d => d.region == t.value).map( k => k.direction))];
+        this.filtreactive.region = t.value;
         break;
       case 'directions':
         this.mycommunes = [...new Set(this.mylistEtablis.filter( d => d.direction == t.value).map( k => k.commune))];
+        this.filtreactive.direction = t.value;
         break;
       case 'communes':
           this.myecoles = this.mylistEtablis.filter( d => d.commune == t.value).map( k => k.ecole);
+          this.filtreactive.commune = t.value;
+          break;
+      case 'ecoles' :
+          this.filtreactive.ecole = t.value;
     }
   }
 
-  find() {
-
-  }
+  find(nmassar) {
+    this.service.setsuspender();
+    const f = [];
+    if (nmassar != '') {
+          this.dataview = this.dataeleves.filter( i => i.nmassar == nmassar);
+          return false;
+        }
+    this.dataview = this.dataeleves.filter( i => {
+        const ad = [];
+        if (this.filtreactive.ecole != ''){
+          ad.push(i.ecole.replace(/ /g, '')  == this.filtreactive.ecole.replace(/ /g, '')) ;
+        }
+        if ( this.filtreactive.direction != '') {
+        ad.push(i.direction.replace(/ /g, '')  == this.filtreactive.direction.replace(/ /g, '') ) ;
+      }
+        if (this.filtreactive.commune != '') {
+        ad.push(i.commune.replace(/ /g, '')  == this.filtreactive.commune.replace(/ /g, '') ) ;
+      }
+        if (this.filtreactive.region != ''){
+          ad.push( i.region.replace(/ /g, '')  == this.filtreactive.region.replace(/ /g, '') ) ;
+        }
+        return ad.indexOf(false) == -1;
+      });
+    }
 }
